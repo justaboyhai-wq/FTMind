c8befbdd4e880e206aa2da49389ff09f8eb2c981
王瓶盖 <962700571@qq.com>
2026-08-19 15:42:55 +0800

    feat: scaffold Baoan policy collector

---
+++ plugins/baoan-policy-collector/go.mod
@@ -0,0 +1,3 @@
+module github.com/justaboyhai-wq/fmind/plugins/baoan-policy-collector
+
+go 1.26.0
---
+++ plugins/baoan-policy-collector/internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import "time"
+
+type Config struct {
+	SeedURL            string
+	DataDir            string
+	AllowedHosts       []string
+	RequestInterval    time.Duration
+	HTMLMaxBytes       int64
+	AttachmentMaxBytes int64
+	PolicyAttachmentsMaxBytes int64
+	RequestTimeout     time.Duration
+	RetryCount         int
+	IncrementalCron    string
+	FullCron           string
+}
+
+func Default() Config {
+	return Config{
+		SeedURL: "https://www.baoan.gov.cn/xxgk/fgk/",
+		DataDir: "./baoan-policy-data",
+		AllowedHosts: []string{"www.baoan.gov.cn"},
+		RequestInterval: time.Second,
+		HTMLMaxBytes: 10 << 20,
+		AttachmentMaxBytes: 100 << 20,
+		PolicyAttachmentsMaxBytes: 500 << 20,
+		RequestTimeout: 30 * time.Second,
+		RetryCount: 3,
+		IncrementalCron: "0 2 * * *",
+		FullCron: "0 3 * * 0",
+	}
+}
